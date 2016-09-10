a22b7b84158e40eafb15f3e24bca02f2ac152b39
Andrew <wiley.a@gmail.com>
2016-09-10 13:33:23 +0100

    [~] Added the tile selection logic.

--- Words_Unity/Assets/Scripts/CharacterBackground.cs
+++ Words_Unity/Assets/Scripts/CharacterBackground.cs
@@ -1,5 +1,56 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CharacterBackground : MonoBehaviour
+	, IEventSystemHandler
+	, IPointerUpHandler
+	, IPointerDownHandler
+	, IPointerEnterHandler
+	, IPointerExitHandler
 {
+	static public GameObject From;
+	static public GameObject To;
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		if (eventData.button == PointerEventData.InputButton.Left)
+		{
+			From = transform.parent.gameObject;
+		}
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		if (eventData.button == PointerEventData.InputButton.Left)
+		{
+			Debug.Log(string.Format("From: {0} To: {1}", From.name, To.name));
+			From = null;
+			To = null;
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		if (eventData.button == PointerEventData.InputButton.Left)
+		{
+			To = transform.parent.gameObject;
+		}
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		if (eventData.button == PointerEventData.InputButton.Left)
+		{
+			To = From;
+		}
+	}
+
+	public void OnDrawGizmosSelected()
+	{
+		if (From && To)
+		{
+			Gizmos.color = Color.magenta;
+			Gizmos.DrawLine(From.transform.position, To.transform.position);
+		}
+	}
 }
