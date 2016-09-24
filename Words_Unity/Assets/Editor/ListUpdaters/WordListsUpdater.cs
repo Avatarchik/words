fa28fd44fe3bbfbd0394f4a77d31219fbf5cafaa
Andrew <wiley.a@gmail.com>
2016-09-24 14:27:57 +0100

    [~] Optimised the word list so it only fetches words under a certain length.

--- Words_Unity/Assets/Editor/ListUpdaters/WordListsUpdater.cs
+++ Words_Unity/Assets/Editor/ListUpdaters/WordListsUpdater.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEditor;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -14,17 +15,26 @@ public class WordsUpdater
 			Words words = wordListsPrefab.GetComponent<Words>();
 			if (words)
 			{
-				string[] wordListPaths = Directory.GetFiles(PathHelper.Combine(Application.dataPath, "Words"), "*.txt");
+				words.Initialise();
+
+				string progressBarMessageFormat = "Processing '{0}' word list.";
+				string progressBarMessage = string.Format(progressBarMessageFormat, "?");
+				ProgressBarHelper.Begin(false, "Word List Updater", progressBarMessage, 1f / 26);
 
 				int wordCount = 0;
 
+				string[] wordListPaths = Directory.GetFiles(PathHelper.Combine(Application.dataPath, "Words"), "*.txt");
 				foreach (string path in wordListPaths)
 				{
+					char initialChar = Path.GetFileNameWithoutExtension(path)[0];
+
+					progressBarMessage = string.Format(progressBarMessageFormat, initialChar);
+					ProgressBarHelper.Update(progressBarMessage);
+
 					string fileContents = File.ReadAllText(path);
 					string[] splitFileContents = fileContents.Split('\n');
-					int splitFileContentsLength = splitFileContents.Length;
 
-					List<string> wordList = new List<string>(splitFileContentsLength);
+					List<string> wordList = new List<string>(splitFileContents.Length);
 					foreach (string word in splitFileContents)
 					{
 						if (IsWordValid(word))
@@ -34,9 +44,10 @@ public class WordsUpdater
 						}
 					}
 
-					string letter = Path.GetFileNameWithoutExtension(path);
-					words.SetList(letter, wordList.ToArray());
+					words.SetList(initialChar, wordList);
 				}
+
+				ProgressBarHelper.End();
 
 				EditorUtility.SetDirty(wordListsPrefab);
 
--- Words_Unity/Assets/Scripts/Words.cs
+++ Words_Unity/Assets/Scripts/Words.cs
@@ -1,200 +1,88 @@
 using UnityEngine;
+using System;
 using System.Collections.Generic;
 
 public class Words : MonoBehaviour
 {
-	// TODO - this needs improving
-
-	public string[] A;
-	public string[] B;
-	public string[] C;
-	public string[] D;
-	public string[] E;
-
-	public string[] F;
-	public string[] G;
-	public string[] H;
-	public string[] I;
-	public string[] J;
-
-	public string[] K;
-	public string[] L;
-	public string[] M;
-	public string[] N;
-	public string[] O;
-
-	public string[] P;
-	public string[] Q;
-	public string[] R;
-	public string[] S;
-	public string[] T;
-
-	public string[] U;
-	public string[] V;
-	public string[] W;
-	public string[] X;
-	public string[] Y;
-
-	public string[] Z;
-
-	public string[] GetList(string letter)
+	[Serializable]
+	public class WordListPortion
 	{
-		switch (letter)
+		public char InitialChar;
+		public int WordLength;
+		public int StartIndex;
+		public int EndIndex;
+		public int ContainedWordsCount;
+
+		public WordListPortion(char initialChar, int wordLength, int startIndex, int endIndex)
 		{
-			case "A": return A;
-			case "B": return B;
-			case "C": return C;
-			case "D": return D;
-			case "E": return E;
-
-			case "F": return F;
-			case "G": return G;
-			case "H": return H;
-			case "I": return I;
-			case "J": return J;
-
-			case "K": return K;
-			case "L": return L;
-			case "M": return M;
-			case "N": return N;
-			case "O": return O;
-
-			case "P": return P;
-			case "Q": return Q;
-			case "R": return R;
-			case "S": return S;
-			case "T": return T;
-
-			case "U": return U;
-			case "V": return V;
-			case "W": return W;
-			case "X": return X;
-			case "Y": return Y;
-
-			case "Z": return Z;
-		}
-
-		Debug.LogError("Failed to find list for letter: " + letter);
-		return null;
-	}
-
-	public void SetList(string letter, string[] words)
-	{
-		switch (letter)
-		{
-			case "A": A = words; break;
-			case "B": B = words; break;
-			case "C": C = words; break;
-			case "D": D = words; break;
-			case "E": E = words; break;
-
-			case "F": F = words; break;
-			case "G": G = words; break;
-			case "H": H = words; break;
-			case "I": I = words; break;
-			case "J": J = words; break;
-
-			case "K": K = words; break;
-			case "L": L = words; break;
-			case "M": M = words; break;
-			case "N": N = words; break;
-			case "O": O = words; break;
-
-			case "P": P = words; break;
-			case "Q": Q = words; break;
-			case "R": R = words; break;
-			case "S": S = words; break;
-			case "T": T = words; break;
-
-			case "U": U = words; break;
-			case "V": V = words; break;
-			case "W": W = words; break;
-			case "X": X = words; break;
-			case "Y": Y = words; break;
-
-			case "Z": Z = words; break;
-
-			default:
-				Debug.LogError("Failed to find list for letter: " + letter);
-				break;
+			InitialChar = initialChar;
+			WordLength = wordLength;
+			StartIndex = startIndex;
+			EndIndex = endIndex;
+			ContainedWordsCount = EndIndex - StartIndex;
 		}
 	}
 
-	public int GetWordCount()
+	public int WordCount;
+	public List<WordListPortion> ListPortions;
+	public string[] WordList;
+
+	public void Initialise()
 	{
-		int count = 0;
-
-		count += A.Length;
-		count += B.Length;
-		count += C.Length;
-		count += D.Length;
-		count += E.Length;
-
-		count += F.Length;
-		count += G.Length;
-		count += H.Length;
-		count += I.Length;
-		count += J.Length;
-
-		count += K.Length;
-		count += L.Length;
-		count += M.Length;
-		count += N.Length;
-		count += O.Length;
-
-		count += P.Length;
-		count += Q.Length;
-		count += R.Length;
-		count += S.Length;
-		count += T.Length;
-
-		count += U.Length;
-		count += V.Length;
-		count += W.Length;
-		count += X.Length;
-		count += Y.Length;
-
-		count += Z.Length;
-
-		return count;
+		WordCount = 0;
+		ListPortions = new List<WordListPortion>();
+		WordList = new string[0];
 	}
 
-	public string[] GetAllWords()
+	public void SetList(char initialChar, List<string> words)
 	{
-		List<string> words = new List<string>(GetWordCount());
+		int additionalWordsCount = words.Count;
 
-		words.AddRange(A);
-		words.AddRange(B);
-		words.AddRange(C);
-		words.AddRange(D);
-		words.AddRange(E);
+		Array.Resize(ref WordList, WordCount + additionalWordsCount);
 
-		words.AddRange(F);
-		words.AddRange(G);
-		words.AddRange(H);
-		words.AddRange(I);
-		words.AddRange(J);
+		words.Sort((a, b) => a.Length.CompareTo(b.Length));
 
-		words.AddRange(K);
-		words.AddRange(L);
-		words.AddRange(M);
-		words.AddRange(N);
-		words.AddRange(O);
+		int desiredLength = 3;
+		int placedWords = 0;
+		List<string> newWords = new List<string>(additionalWordsCount);
+		while (placedWords < additionalWordsCount)
+		{
+			newWords.Clear();
+			foreach (string word in words)
+			{
+				if (word.Length == desiredLength)
+				{
+					newWords.Add(word);
+				}
+			}
+			newWords.Sort();
 
-		words.AddRange(P);
-		words.AddRange(Q);
-		words.AddRange(R);
-		words.AddRange(S);
-		words.AddRange(T);
+			int newWordsCount = newWords.Count;
+			Array.Copy(newWords.ToArray(), 0, WordList, WordCount, newWordsCount);
 
-		words.AddRange(U);
-		words.AddRange(V);
-		words.AddRange(W);
-		words.AddRange(X);
-		words.AddRange(Y);
+			ListPortions.Add(new WordListPortion(initialChar, desiredLength, WordCount, WordCount + newWordsCount));
+			WordCount += newWordsCount;
+			placedWords += newWordsCount;
 
-		words.AddRange(Z);
+			++desiredLength;
+		}
+	}
 
-		return words.ToArray();
+	public string[] GetAllWords(int maxWordLength)
+	{
+		string[] foundWords = new string[WordCount];
+		int foundWordsCount = 0;
+
+		foreach (WordListPortion portion in ListPortions)
+		{
+			if (portion.WordLength <= maxWordLength)
+			{
+				Array.Copy(WordList, portion.StartIndex, foundWords, foundWordsCount, portion.ContainedWordsCount);
+				foundWordsCount += portion.ContainedWordsCount;
+			}
+		}
+
+		Array.Resize(ref foundWords, foundWordsCount);
+		return foundWords;
 	}
 }
