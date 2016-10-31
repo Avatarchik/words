915db34e1b012a1cff78bc34ef1af0e5b7cfe393
Andrew <wiley.a@gmail.com>
2016-10-31 21:25:02 +0000

    [~] The button colours can now be toggled on and off, rather than a blanket set.

--- Words_Unity/Assets/Scripts/Menus/ButtonColourSetter.cs
+++ Words_Unity/Assets/Scripts/Menus/ButtonColourSetter.cs
@@ -6,14 +6,29 @@ public class ButtonColourSetter : MonoBehaviour
 	public Color ButtonColour;
 	private Button mButtonRef;
 
+	public bool SetHighlightedColour = true;
+	public bool SetPressedColour = true;
+
 	void Awake()
 	{
+#if UNITY_ANDROID || UNITY_IOS
+		SetHighlightedColour = false;
+#endif // UNITY_ANDROID || UNITY_IOS
+
 		mButtonRef = GetComponent<Button>();
 		ODebug.AssertNull(mButtonRef);
 
 		ColorBlock colourBlock = mButtonRef.colors;
-		colourBlock.highlightedColor = ButtonColour;
-		colourBlock.pressedColor = ButtonColour;
+
+		if (SetHighlightedColour)
+		{
+			colourBlock.highlightedColor = ButtonColour;
+		}
+		if (SetPressedColour)
+		{
+			colourBlock.pressedColor = ButtonColour;
+		}
+
 		mButtonRef.colors = colourBlock;
 	}
 }
