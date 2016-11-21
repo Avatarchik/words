ee86451c482eb036a8e263c839a9833a626eb8fa
Andrew <wiley.a@gmail.com>
2016-11-21 20:43:18 +0000

    [+-] Added the placeholder setup for the stats menu.

--- Words_Unity/Assets/Scripts/Managers/MenuManager.cs
+++ Words_Unity/Assets/Scripts/Managers/MenuManager.cs
@@ -14,6 +14,7 @@ public enum EMenuType
 	PuzzleResetMenu,
 	OptionsMenu,
 	ProgressResetMenu,
+	StatsMenu,
 
 	Count,
 }
---
+++ Words_Unity/Assets/Scripts/Managers/StatsManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System;
+
+/*
+[Serializable]
+public class ColourScheme : ScriptableObject
+{
+	public string Name;
+	public Color High;
+	public Color Low;
+	public Color Highlight;
+}*/
+
+public class StatsManager : SingletonMonoBehaviour<StatsManager>
+{
+	/*
+	 *	 *	need:
+	 *	 *		total words
+	 *	 *		total puzzles
+	 *	 *	 
+	 *	 *	 words found
+	 *	 *	 puzzles complete
+	 *	 *	
+	 *	 *	 words checked
+	 *	 *		total &
+	 *	 *		by puzzle size
+	 *	 *	 score achieved
+	 *	 *		total &
+	 *	 *		by puzzle size
+	 *	 *	 time played
+	 *	 *		total &
+	 *	 *		by puzzle size
+	 *	 *	 
+	 *	 *	 favourite colour scheme
+	 */
+}
