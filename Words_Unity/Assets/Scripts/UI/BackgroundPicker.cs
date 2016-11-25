a46e360343fc09a6fd52a29aead1579f28c0a951
Andrew <wiley.a@gmail.com>
2016-11-25 17:11:28 +0000

    [~] Fixed the menu backgrounds that are bands which were scaling to full screen when they shouldn't have been.

--- Words_Unity/Assets/Scripts/UI/BackgroundPicker.cs
+++ Words_Unity/Assets/Scripts/UI/BackgroundPicker.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [ScriptOrder(-50)]
-public class BackgroundPicker : ScreenStretchedUIMonoBehaviour
+public class BackgroundPicker : UIMonoBehaviour, IOrientationChangedNotifiee
 {
 	public Image ImageRef;
 	public Sprite[] Backgrounds;
 	static public Sprite sChosenBackground { get; private set; }
+
+	public bool IsABand = false;
 
 	void Awake()
 	{
@@ -17,5 +19,27 @@ public class BackgroundPicker : ScreenStretchedUIMonoBehaviour
 		}
 
 		ImageRef.sprite = sChosenBackground;
+	}
+
+	public virtual void OnEnable()
+	{
+		OrientationManager.Instance.RegisterForNotification(this);
+	}
+
+	public virtual void OnDisable()
+	{
+		OrientationManager.Instance.UnregisterForNotification(this);
+	}
+
+	public void OnScreenSizeChanged(Vector2 screenSize)
+	{
+		if (IsABand)
+		{
+			rectTransform.sizeDelta = new Vector2(screenSize.x, rectTransform.sizeDelta.y);
+		}
+		else
+		{
+			rectTransform.sizeDelta = screenSize;
+		}
 	}
 }
