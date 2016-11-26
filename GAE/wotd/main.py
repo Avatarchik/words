5264abfeedfcd21321a3c5bbc03291ac2467faaf
Andrew <wiley.a@gmail.com>
2016-11-26 21:15:22 +0000

    [~] Testing GET with GAE.

--- GAE/wotd/main.py
+++ GAE/wotd/main.py
@@ -1,15 +1,26 @@
 from flask import Flask
+import datetime
+from google.appengine.ext import webapp
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
 @app.route('/')
 def hello():
+	# GET
+	x = MyHandler()
+	print x.get()
+
     """Return a friendly HTTP greeting."""
-    return 'Hello World! It Works! test 123'
+    return 'Hello World! It Works! test 456'
+
+class MyHandler(webapp.RequestHandler):
+    def get(self):
+        name1 = self.request.get_all("thing")
+        return name1
 
 
 @app.errorhandler(404)
