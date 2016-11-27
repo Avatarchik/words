ce611772603b99e71bb90e2243fa8a39e8b71d00
Andrew <wiley.a@gmail.com>
2016-11-27 02:01:20 +0000

    [~] Query format change.

--- GAE/wotd/main.py
+++ GAE/wotd/main.py
@@ -4,25 +4,22 @@ import webapp2
 from google.appengine.ext import ndb
 
 class WOTD(ndb.Model):
-
-	day = ndb.IntegerProperty(indexed=True)
-	word = ndb.StringProperty(indexed=False)
-	definition = ndb.StringProperty(indexed=False)
+	day = ndb.IntegerProperty()
+	word = ndb.StringProperty()
+	definition = ndb.StringProperty()
 
 class PageNotFound(webapp2.RequestHandler):
-
 	def get(self):
 		self.response.write('Page not found!')
 
 class GetWOTD(webapp2.RequestHandler):
-
 	def get(self):
 		day = int(self.request.get('day', -1))
 		self.response.write(day)
 
 		if day != -1:
 			self.response.write("day is valid")
-			todays_wotd = WOTD.query(WOTD.day == day).get()
+			todays_wotd = WOTD.query().filter(WOTD.day == day).get()
 			if todays_wotd:
 				self.response.write("today's wotd is valid")
 				self.response.write(todays_wotd.day)
