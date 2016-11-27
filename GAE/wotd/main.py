29e88ead813f8960a4dc5a2bf0963e26571c19ed
Andrew <wiley.a@gmail.com>
2016-11-27 02:27:00 +0000

    [~] Removed most of the debug logs, renamed day to daystamp and testing how the data can be returned.

--- GAE/wotd/main.py
+++ GAE/wotd/main.py
@@ -4,7 +4,7 @@ import webapp2
 from google.appengine.ext import ndb
 
 class WOTD(ndb.Model):
-	day = ndb.IntegerProperty()
+	daystamp = ndb.IntegerProperty()
 	word = ndb.StringProperty()
 	definition = ndb.StringProperty()
 
@@ -14,20 +14,13 @@ class PageNotFound(webapp2.RequestHandler):
 
 class GetWOTD(webapp2.RequestHandler):
 	def get(self):
-		day = int(self.request.get('day', -1))
-		self.response.write(day)
+		daystamp = int(self.request.get('daystamp', -1))
 
-		if day != -1:
-			self.response.write("day is valid")
-			todays_wotd = WOTD.query(WOTD.day == day).get()
+		if daystamp != -1:
+			todays_wotd = WOTD.query(WOTD.daystamp == daystamp).get()
 			if todays_wotd:
-				self.response.write("today's wotd is valid")
-				self.response.write(todays_wotd.day)
-				self.response.write(todays_wotd.word)
-				self.response.write(todays_wotd.definition)
+				self.response.write(todays_wotd)
 				return
-			else:
-				self.response.write("today's wotd is invalid")
 		
 		self.response.write('UNKNOWN')
 
