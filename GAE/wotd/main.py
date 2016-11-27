89aa117fcbbb18ecc855a6b4e2f1d80a4e342962
Andrew <wiley.a@gmail.com>
2016-11-27 00:25:01 +0000

    [~] Removed an if statement for now as I don't think it was correct.

--- GAE/wotd/main.py
+++ GAE/wotd/main.py
@@ -24,11 +24,10 @@ class WOTD(webapp2.RequestHandler):
 			self.response.write("day is valid")
 			todays_wotd_query = WordDefinition.query(WordDefinition.day == day)
 			todays_wotd = todays_wotd_query.fetch(1)
-			if todays_wotd:
-				self.response.write(todays_wotd[0].day)
-				self.response.write(todays_wotd[0].word)
-				self.response.write(todays_wotd[0].definition)
-				return
+			self.response.write(todays_wotd[0].day)
+			self.response.write(todays_wotd[0].word)
+			self.response.write(todays_wotd[0].definition)
+			return
 		
 		self.response.write('UNKNOWN')
 
