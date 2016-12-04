95a77dac0abb2278eeac4c9440a83c697a892e48
Andrew <wiley.a@gmail.com>
2016-12-04 18:56:14 +0000

    [~] SerializableGuid now correctly handles null during comparisons.

--- Words_Unity/Assets/Scripts/SerializableGuid.cs
+++ Words_Unity/Assets/Scripts/SerializableGuid.cs
@@ -4,12 +4,19 @@ using System;
 [Serializable]
 public class SerializableGuid
 {
+	static public SerializableGuid Empty = new SerializableGuid(Guid.Empty);
+
 	[SerializeField]
-	public string Value;
+	public string Value = string.Empty;
 
 	public SerializableGuid()
 	{
 		Value = Guid.NewGuid().ToString();
+	}
+
+	public SerializableGuid(Guid guid)
+	{
+		Value = guid.ToString();
 	}
 
 	public static implicit operator SerializableGuid(Guid rhs)
@@ -37,6 +44,40 @@ public class SerializableGuid
 		{
 			return Guid.Empty;
 		}
+	}
+
+	static public bool operator ==(SerializableGuid lhs, SerializableGuid rhs)
+	{
+		if (!object.ReferenceEquals(lhs, null) && !object.ReferenceEquals(rhs, null))
+		{
+			bool areEqual = lhs.Value == rhs.Value;
+			return areEqual;
+		}
+
+		return false;
+	}
+
+	static public bool operator !=(SerializableGuid lhs, SerializableGuid rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	public override bool Equals(object obj)
+	{
+		if (obj == null)
+		{
+			return false;
+		}
+
+		SerializableGuid rhs = (SerializableGuid)obj;
+		bool areEqual = Value == rhs.Value;
+		return areEqual;
+	}
+
+	public override int GetHashCode()
+	{
+		// TODO - not sure if this is correct
+		return -1;
 	}
 
 	public override string ToString()
